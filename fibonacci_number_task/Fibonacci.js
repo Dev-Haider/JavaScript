f2bea5affab7c55b5fdf43a558ffb7cee588d8d4
haider.json <144625706+Dev-Haider@users.noreply.github.com>
2025-09-18 20:51:46 +0500

    task

---
+++ fibonacci_number_task/Fibonacci.js
@@ -0,0 +1,18 @@
+function fibonacci (n) {
+    let f1=0, f2=1, next;
+console.log(f1);
+console.log(f2);
+
+for(let i=2; i <n; i++)
+
+{
+next=f1 + f2;
+console.log(next);
+f1=f2;
+f2=n 
+  }
+} 
+fibonacci(10);
+
+
+
---
+++ fibonacci_number_task/readme.md
@@ -0,0 +1,3 @@
+Fibonacci Sequence in JavaScript
+
+This project demonstrates a simple JavaScript function that prints the Fibonacci sequence up to a given number of terms.
